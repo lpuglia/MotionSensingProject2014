b044f9117d4e05ee7ef33a6034f9b3889e4f4e24
Sguglielmini <sabino.guglielmini@gmail.com>
2014-07-09 22:58:13 +0200

    Create motion.h

---
+++ Motion/motion.h
@@ -0,0 +1,48 @@
+/*
+ * motion.h
+ */
+
+#ifndef MOTION_H_
+#define MOTION_H_
+
+#include "hal.h"
+#include "ch.h"
+
+
+//  DEFINIZIONE STRUTTURA gpiopin
+typedef struct gpiopin_t{
+  ioportid_t port;
+  ioportmask_t pin;
+} GPIO_PIN;
+
+//  DEFINIZIONE STRUTTURA Motor
+typedef struct Motor_t{
+    // pin GPIO a cui e' connesso DCMotor IN1
+    GPIO_PIN IN1;
+    // pin GPIO a cui e' connesso DCMotor IN2
+    GPIO_PIN IN2;
+    // pin GPIO a cui e' connesso DCMotor EN
+    GPIO_PIN EN;
+    //  driver PWM utilizzato con EN
+    PWMDriver *pwm_driver;
+    // canale PWM utilizzato con EN
+    pwmchannel_t pwm_channel;
+} DCMotor;
+
+/****************************************************************************
+ *  Funzioni Motori
+ ****************************************************************************/
+
+ //init
+ void DCMotorConfig(DCMotor *dcmotor);
+
+ //comandi
+ void DCMotorForward(DCMotor *dcmotor, int speedPercent);
+ void DCMotorBackward(DCMotor *dcmotor, int speedPercent);
+ void DCMotorStop(DCMotor *dcmotor);
+ void DCMotorFree(DCMotor *dcmotor);
+ void DCMotorTurnR(DCMotor *dcmotor1, DCMotor *dcmotor2, int speedPercent);
+ void DCMotorTurnL(DCMotor *dcmotor1, DCMotor *dcmotor2, int speedPercent);
+
+
+#endif /* MOTION_H_ */
