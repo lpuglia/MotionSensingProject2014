1e5b97669d5c87eff25a8b0c96637eca8d910214
jacekfilipczuk <jacekfilipczuk89@gmail.com>
2014-12-09 11:50:35 +0100

    Android application

---
+++ Android/main.c
@@ -0,0 +1,215 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+#include "ch.h"
+#include "hal.h"
+#include "motion.c"
+
+/*
+ * Parametri configurazione Access Point
+ */
+static const char* AT_CHANNEL = "at+channel=6\r\n";
+static const char* AT_IPSTATIC = "at+ipstatic=192.168.100.1,255.255.255.0,192.168.100.1,0,0\r\n";
+static const char* AT_IPDHCP = "at+ipdhcp=1\r\n";
+static const char* AT_AP = "at+ap=MotionSensing2014,0\r\n";
+
+/*
+ * Parametri configurazione motore di movimento
+ */
+unsigned int motor1Pin1 = 1;    // pin 2 on L293D
+unsigned int motor1Pin2 = 2;    // pin 7 on L293D
+unsigned int enableMotor1 = 0;  // pin 1 on L293D
+unsigned int motor2Pin1 = 7;    // pin 10 on L293D
+unsigned int motor2Pin2 = 8;    // pin 15 on L293D
+unsigned int enableMotor2 = 6;  // pin 9 on L293D
+
+
+
+/*
+ * Parametri configurazione TCP Server
+ */
+static const char* AT_LTCP = "at+ltcp=25000\r\n";
+
+/*
+ * Transparent transmission mode
+ */
+static const char* AT_PWRMODE = "at+pwrmode=0\r\n";
+static const char* AT_EASY = "at+easy_txrx\r\n";
+
+
+#define MAX_LENGTH  1400 //lunghezza max messaggi trasmessi dal modulo WiFi, si potrebbe ridurre
+uint8_t buff[MAX_LENGTH]; //buffer di lettura
+
+
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /*
+   * strutture per il movimento (i motori)
+   */
+  DCMotor motor1 = {
+        {GPIOB, motor1Pin1},     // motor1 IN1 : PB1
+        {GPIOB, motor1Pin2},     // motor1 IN2 : PB2
+        {GPIOB, enableMotor1},   // motor1 EN  : PB0
+        &PWMD3,                  // motor1 EN PWM Driver : TIM3
+        2                        // motor1 EN PWM Channel : CH3
+    };
+
+    DCMotor motor2 = {
+        {GPIOB, motor2Pin1},     // motor2 IN1 : PB7
+        {GPIOB, motor2Pin2},     // motor2 IN2 : PB8
+        {GPIOB, enableMotor2},   // motor2 EN  : PB6
+        &PWMD4,                  // motor2 EN PWM Driver : TIM4
+        0                        // motor2 EN PWM Channel : CH1
+    };
+
+
+  /*
+   * Activates the serial driver 1, PA9 and PA10 are routed to USART1.
+   */
+  sdStart(&SD1, NULL);
+  palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));       /* USART1 TX.       */
+  palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));      /* USART1 RX.       */
+
+
+  sdRead(&SD1,  buff, 1);
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_CHANNEL);
+  sdRead(&SD1,  buff, 4);
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_IPSTATIC);
+  sdRead(&SD1,  buff, 4);
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_IPDHCP);
+  sdRead(&SD1,  buff, 4);
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_AP);
+  sdRead(&SD1,  buff, 4);
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_LTCP);
+  sdRead(&SD1,  buff, 5);
+  palSetPad(GPIOE, GPIOE_LED7_GREEN); //da ora e' possibile connettere un client
+
+  /*
+   * Segue una read bloccante che attende il messaggio che annuncia
+   * la connessione di un client, possibile sostiture con un sistema di lettura
+   * asincrono.
+   */
+  sdRead(&SD1,  buff, 23);
+
+  palClearPad(GPIOE, GPIOE_LED7_GREEN);
+  /*
+   * Viene attivata la transparent transmission mode, in cui i
+   * messaggi vengono inoltrati direttamente a server e client senza
+   * invocare esplicitamente send/recv data
+   */
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_PWRMODE);
+  sdRead(&SD1,  buff, 4);
+  chprintf((BaseSequentialStream *)&SD1, "%s", AT_EASY);
+  sdRead(&SD1,  buff, 4);
+  chprintf((BaseSequentialStream *)&SD1, "prova di easy transmission\r\n"); //messaggio per il client TCP
+
+  *buff = 0;
+
+  char *ptr = buff; //i char sono unsigned per default
+  EventListener el;
+  flagsmask_t flags;
+  chEvtRegisterMask((EventSource *)chnGetEventSource(&SD1), &el, EVENT_MASK(1));
+  int vel=90;
+  /*
+   * Il server attende linee terminate da \r\n
+   * e le rispedisce al client
+   */
+  while (TRUE) {
+
+    //echo singolo carattere
+    //int input = sdGet(&SD1);
+    //chprintf((BaseSequentialStream *)&SD1, "%c", input);
+
+    chEvtWaitOneTimeout(EVENT_MASK(1), MS2ST(10));
+    flags = chEvtGetAndClearFlags(&el);
+    if (flags & CHN_INPUT_AVAILABLE)
+    {
+      int charbuf;
+      char* substr;
+      do
+      {
+        charbuf = chnGetTimeout(&SD1, TIME_IMMEDIATE);
+        if ( charbuf != Q_TIMEOUT )
+        {
+          if (charbuf == '\r')
+          {
+            if ((charbuf = sdGet(&SD1)) == '\n')
+            {
+              //Scelta dell'operazione da eseguire
+              switch(buff[0])
+              {
+              case 'U': //UP
+                              DCMotorForward(&motor1, vel);
+                              DCMotorForward(&motor2, vel);
+                              break;
+              case 'D': //DOWN
+                              DCMotorBackward(&motor1, vel);
+                              DCMotorBackward(&motor2, vel);
+                              break;
+              case 'R': //RIGHT
+                              DCMotorTurnR(&motor1,&motor2, vel);
+                              break;
+              case 'L': //LEFT
+                              DCMotorTurnL(&motor1, &motor2, vel);
+                              break;
+              case 'V': //VELOCITY
+
+                              strncpy(substr, buff+9, strlen(buff)-9);
+                              vel=atoi(substr);
+                              break;
+              default:
+                              DCMotorStop(&motor1);
+                              DCMotorStop(&motor2);
+                              break;
+              }
+
+              *ptr = 0;
+              chprintf((BaseSequentialStream*)&SD1, "%s\r\n", buff);
+              *buff=0;
+              ptr = buff;
+            }
+            else
+            {
+              *ptr++ = '\r';
+              *ptr++ = charbuf;
+            }
+          }
+          else
+          {
+            *ptr++ = charbuf;
+          }
+        }
+      }
+      while (charbuf != Q_TIMEOUT);
+    }
+
+
+  }
+}
