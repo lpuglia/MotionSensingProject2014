b1c8ed16870675af8c0bf992b418a36911de4fe3
paola-g <paola.galdi@gmail.com>
2014-12-19 17:03:24 +0100

    integrazione
    aggiunti file progetto integrato

---
+++ Integration/motion.c
@@ -0,0 +1,133 @@
+/*
+ * motion.c
+ *
+ *  Created on: 05/dic/2014
+ *      Author: Jacek
+ */
+
+
+
+#include "ch.h"
+#include "hal.h"
+#include "motion.h"
+
+
+// STRUTTURA pwmcfg
+static PWMConfig pwmcfg = {
+   1000000, // 1MHz PWM clock frequency
+   10000,   // PWM period 10 milliseconds
+   NULL,    // no callback
+   {
+    // PWM channels array
+      {PWM_OUTPUT_DISABLED, NULL},   // Channel 0
+      {PWM_OUTPUT_DISABLED, NULL},   // Channel 1
+      {PWM_OUTPUT_DISABLED, NULL},   // Channel 2
+      {PWM_OUTPUT_DISABLED, NULL}    // Channel 3
+   },
+   0,
+   0
+};
+
+
+// Get getPwmPercentage
+static int getPwmPercentage(int speedPercent) {
+    int p = speedPercent > 100 ? 100 : (speedPercent < 40 ? 0 : speedPercent);
+    return  p*100;
+}
+
+
+/****************************************************************************
+ *  Funzioni Motori
+ ****************************************************************************/
+
+
+void DCMotorConfig(DCMotor *dcmotor) {
+
+   // enable pwm channel
+   PWMChannelConfig chcfg = {PWM_OUTPUT_ACTIVE_HIGH, NULL};
+    pwmcfg.channels[dcmotor->pwm_channel] = chcfg;
+
+    /* configurazione PAL */
+    palSetPadMode(dcmotor->IN1.port, dcmotor->IN1.pin, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(dcmotor->IN2.port, dcmotor->IN2.pin, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(dcmotor->EN.port, dcmotor->EN.pin, PAL_MODE_ALTERNATE(2));
+
+    /* start PWM  */
+    pwmStart(dcmotor->pwm_driver, &pwmcfg);
+}
+
+void DCMotorStop(DCMotor *dcmotor) {
+    palClearPad(dcmotor->IN1.port, dcmotor->IN1.pin);
+    palClearPad(dcmotor->IN2.port, dcmotor->IN2.pin);
+    // set EN PWM HIGH part as FULL
+    pwmEnableChannel( dcmotor->pwm_driver,
+                      dcmotor->pwm_channel,
+                      PWM_PERCENTAGE_TO_WIDTH(dcmotor->pwm_driver,
+                                              getPwmPercentage(100))
+                     );
+}
+
+void DCMotorFree(DCMotor *dcmotor) {
+    palClearPad(dcmotor->IN1.port, dcmotor->IN1.pin);
+    palClearPad(dcmotor->IN2.port, dcmotor->IN2.pin);
+    pwmEnableChannel( dcmotor->pwm_driver,
+                      dcmotor->pwm_channel,
+                      PWM_PERCENTAGE_TO_WIDTH(dcmotor->pwm_driver, 0)
+                     );
+}
+
+void DCMotorForward(DCMotor *dcmotor, int speedPercent) {
+    palClearPad(dcmotor->IN1.port, dcmotor->IN1.pin);
+    palSetPad(dcmotor->IN2.port, dcmotor->IN2.pin);
+    // set speed
+    pwmEnableChannel( dcmotor->pwm_driver,
+                      dcmotor->pwm_channel,
+                      PWM_PERCENTAGE_TO_WIDTH(dcmotor->pwm_driver,
+                                              getPwmPercentage(speedPercent))
+                     );
+}
+
+void DCMotorBackward(DCMotor *dcmotor, int speedPercent) {
+    palSetPad(dcmotor->IN1.port, dcmotor->IN1.pin);
+    palClearPad(dcmotor->IN2.port, dcmotor->IN2.pin);
+    // set speed
+    pwmEnableChannel( dcmotor->pwm_driver,
+                      dcmotor->pwm_channel,
+                      PWM_PERCENTAGE_TO_WIDTH( dcmotor->pwm_driver,
+                                               getPwmPercentage(speedPercent)
+                                              )
+                     );
+}
+
+void DCMotorTurnL(DCMotor *dcmotor1, DCMotor *dcmotor2, int speedPercent) {
+
+    DCMotorForward(dcmotor1, speedPercent);
+    chThdSleepMilliseconds(1000);
+    DCMotorForward(dcmotor2, speedPercent-10);
+
+}
+
+void DCMotorTurnR(DCMotor *dcmotor1, DCMotor *dcmotor2, int speedPercent) {
+
+    DCMotorForward(dcmotor1, speedPercent-10);
+    DCMotorForward(dcmotor2, speedPercent);
+    chThdSleepMilliseconds(1000);
+}
+
+void DCMotorSplitL(DCMotor *dcmotor1, DCMotor *dcmotor2, int degree) {
+
+    DCMotorForward(dcmotor1, 100);
+    DCMotorBackward(dcmotor2, 100);
+    chThdSleepMilliseconds((1200*degree)/90);
+}
+
+void DCMotorSplitR(DCMotor *dcmotor1, DCMotor *dcmotor2, int degree) {
+
+    DCMotorBackward(dcmotor1, 100);
+    DCMotorForward(dcmotor2, 100);
+    chThdSleepMilliseconds((1200*degree)/90);
+}
+
+
+
+
---
+++ Integration/motion.h
@@ -0,0 +1,56 @@
+/*
+ * motion.h
+ *
+ *  Created on: 05/dic/2014
+ *      Author: Jacek
+ */
+
+#ifndef MOTION_H_
+#define MOTION_H_
+
+
+#include "hal.h"
+#include "ch.h"
+
+
+//  DEFINIZIONE STRUTTURA gpiopin
+typedef struct gpiopin_t{
+  ioportid_t port;
+  ioportmask_t pin;
+} GPIO_PIN;
+
+//  DEFINIZIONE STRUTTURA Motor
+typedef struct Motor_t{
+    // pin GPIO a cui e' connesso DCMotor IN1
+    GPIO_PIN IN1;
+    // pin GPIO a cui e' connesso DCMotor IN2
+    GPIO_PIN IN2;
+    // pin GPIO a cui e' connesso DCMotor EN
+    GPIO_PIN EN;
+    //  driver PWM utilizzato con EN
+    PWMDriver *pwm_driver;
+    // canale PWM utilizzato con EN
+    pwmchannel_t pwm_channel;
+} DCMotor;
+
+/****************************************************************************
+ *  Funzioni Motori
+ ****************************************************************************/
+
+ //init
+ void DCMotorConfig(DCMotor *);
+
+ //comandi generici
+ void DCMotorForward(DCMotor *, int );
+ void DCMotorBackward(DCMotor *, int );
+ void DCMotorStop(DCMotor *);
+ void DCMotorFree(DCMotor *);
+ //comandi per biciclo
+ void DCMotorTurnR(DCMotor *, DCMotor *, int );
+ void DCMotorTurnL(DCMotor *, DCMotor *, int );
+ void DCMotorSplitL(DCMotor *, DCMotor *, int );
+ void DCMotorSplitR(DCMotor *, DCMotor *, int );
+
+
+
+#endif /* MOTION_H_ */
